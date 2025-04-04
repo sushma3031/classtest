ec16e96a3227d3aaecae0c806ad43cd757bf6741
sushmabhadragoudra@gmail.com <sushmabhadragoudra@gmail.com>
2025-04-03 22:48:33 -0700

    save

---
+++ simpleinterest.java
@@ -0,0 +1,12 @@
+public class simpleinterest{
+    
+    public static void main(String[]args)
+    {
+        int p=2000;
+        int t=3000;
+        int r=500;
+        int SI;
+        SI=(p*t*r)/100;
+        system.out.println("simpleinterest is"+SI);
+    }
+}
