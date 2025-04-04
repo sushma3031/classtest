01d6afc0f67affc406ed700be670fb8f1110db28
sushmabhadragoudra@gmail.com <sushmabhadragoudra@gmail.com>
2025-04-03 19:58:42 -0700

    saved

---
+++ constructor.java
@@ -0,0 +1,23 @@
+class_conschain
+{
+    string name;
+    int roll no;
+    string course;
+    conschain()
+    {
+        name="Ayush";
+        System.out.println("name",name);
+        conschain (String name,int rollno,string course);
+        {
+            this();
+            this.name=name;
+            this.rollno;
+            this.course;
+            system.out.println("name:"+name+"roll no:"+roll no+"course:"+course+);
+        }
+        public static void main(String[]args)
+        {
+            conschain c=new conschain("Arun","100","Bca");
+        }
+    }
+}
