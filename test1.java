fee884ac785d74f2ff3762c820f47a2e93cef94e
sushmabhadragoudra@gmail.com <sushmabhadragoudra@gmail.com>
2025-04-10 21:59:34 -0700

    saved

---
+++ test1.java
@@ -0,0 +1,12 @@
+class test1{
+    public static void main(String[]args){
+        int a=10;
+        int b=20;
+        system.out.println("a>b:"+(a>b));
+        system.out.println("a<b:"+(a<b));
+        system.out.println("a>=b:"+(a>=b));
+        system.out.println("a<=b:"+(a<=b));
+        system.out.println("a!=b:"+(a!=b));
+
+    }
+}
