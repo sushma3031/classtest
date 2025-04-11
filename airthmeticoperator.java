79078e32e704ff1bc49a2e2afff039436e16cb4f
sushmabhadragoudra@gmail.com <sushmabhadragoudra@gmail.com>
2025-04-10 21:19:11 -0700

    saved

---
+++ airthmeticoperator.java
@@ -0,0 +1,11 @@
+public class operator{
+    static int a=20,b=40;
+    {
+        System.out.println("value of addition",a+b);
+        System.out.println("value of subtraction",a-b);
+        System.out.println("value of multiplication",a*b);
+        System.out.println("value of division",a/b);
+        System.out.println("value of modulus",a%b);
+
+    }
+}
